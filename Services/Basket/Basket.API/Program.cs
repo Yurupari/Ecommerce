1960fc7695949b6b3e9761a91b73a0dc3a7f5f15
Cristhian David Marín Angulo <cristhianmarin15@live.com>
2024-11-04 16:00:42 -0500

    Added logic for Basket API to consume Discount API

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,9 @@
 using Asp.Versioning;
+using Basket.Application.GrpcService;
 using Basket.Application.Handlers;
 using Basket.Core.Repositories;
 using Basket.Infraestructure.Repositories;
+using Discount.Grpc.Protos;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -43,6 +45,10 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 // Register Application Service
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddScoped<DiscountGrpcService>();
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
+    cfg => cfg.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"])
+);
 
 var app = builder.Build();
 
--- Services/Basket/Basket.Application/Handlers/CreateShoppingCartCommandHandler.cs
+++ Services/Basket/Basket.Application/Handlers/CreateShoppingCartCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.Application.Commands;
+using Basket.Application.GrpcService;
 using Basket.Application.Mappers;
 using Basket.Application.Responses;
 using Basket.Core.Entities;
@@ -10,15 +11,22 @@ namespace Basket.Application.Handlers
     public class CreateShoppingCartCommandHandler : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
     {
         private readonly IBasketRepository _basketRepository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository)
+        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository, DiscountGrpcService discountGrpcService)
         {
             _basketRepository = basketRepository;
+            _discountGrpcService = discountGrpcService;
         }
 
         public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
         {
-            // TODO: Will be integrating to Discount service
+            foreach (var item in request.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+
             var shoppingCartEntity = BasketMapper.Mapper.Map<ShoppingCart>(request);
             var shoppingCart = await _basketRepository.UpdateBasket(shoppingCartEntity);
 
